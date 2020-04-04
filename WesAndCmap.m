ae03589e831827f4ba48ebba7aca935bc397e5a6
Kris S <37545869+KrisNeuro@users.noreply.github.com>
2020-04-04 18:32:53 -0400

    Update WesAndCmap.m

--- WesAndCmap.m
+++ WesAndCmap.m
@@ -8,28 +8,69 @@
 % *****************************************************************************
 
 %% Wes Anderson color palettes
-  % Converted from hex color code to RGB (to be read by MATLAB). Selected color palettes look discernable in grayscale.
+  % Converted from hex color code to RGB/255 (to be read by MATLAB). Selected color palettes look discernable in grayscale.
 
-GrandBudapest2 = 	[230 160 196;  198 205 247;  216 164 153;  114 148 212];
-Moonrise3 =    		[133 212 227;  244 181 189;  156 150 74;  205 192 140];
-Darjeeling2 = 		[236 203 0174;  4 108 154;  214 156 78; 171 221 222;  0 0 0];
-Rushmore1 = 	   	[225 189 109;  234 190 148;  11 119 94;  53 39 74;  242 48 15];
-Royal1 =          [137 157 164;  201 51 18;  250 239 209;  220 134 59];
-Moonrise1 =  	   	[243 223 108;  206 171 7;  213 213 211; 36 40 26];
+Rushmore1 = 	   	[225 189 109;  234 190 148;  11 119 94;  53 39 74;  242 48 15]/255;
+  maleblue = Rushmore1(3,:); %dark teal green
+  fempurp = Rushmore1(4,:); %deep purple
+
+GrandBudapest2 = 	[230 160 196;  198 205 247;  216 164 153;  114 148 212]/255;
+  %fempurp = GrandBudapest2(3,:);
+  %maleblue = GrandBudapest2(4,:);
+  %maleblue = [2 64 27]/255; % Cavalcanti1(2)
+  digreen = GrandBudapest2(4,:);
+  propurp = GrandBudapest2(1,:);
+  estyel = GrandBudapest2(3,:);
+  metred = GrandBudapest2(2,:);
+
+Moonrise3 =    		[133 212 227;  244 181 189;  156 150 74;  205 192 140; 250 215 123]/255; % don’t use 1&4 together
+  maleblue = Moonrise3(1,:);
+  fempurp = Moonrise3(3,:); 
+  %propurp = Moonrise3(2,:); %baby pink :\
+  %digreen = Moonrise3(3,:);
+  %estyel = Moonrise3(5,:);
+  %metred = Moonrise3(4,:);
+
+Darjeeling2 = 		[236 203 0174;  4 108 154;  214 156 78; 171 221 222;  0 0 0]/255; % don’t use 1&4 together
+  maleblue = Darjeeling2(5,:); %black
+  digreen = Darjeeling2(1,:);
+  propurp = Darjeeling2(2,:);
+  estyel = Darjeeling2(3,:);
+  %metred = Darjeeling2(4,:);
+
+Royal1 =          [137 157 164;  201 51 18;  250 239 209;  220 134 59]/255;
+  digreen = Royal1(1,:);
+  propurp = Royal1(2,:);
+  estyel = Royal1(4,:);
+  metred = Royal1(3,:);
+
+Moonrise1 =  	   	[243 223 108;  206 171 7;  213 213 211; 36 40 26]/255;
+  digreen = Moonrise1(2,:);
+  propurp = Moonrise1(4,:);
+  estyel = Moonrise1(1,:);
+  metred = Moonrise1(3,:);
+
+
 
 % *****************************************************************************
 
 %% colorcet color palettes
   % used in draft figures
-fempurp = colorcet('L8'); %linear blue magenta yellow
-    propurp = fempurp(65,:); %purple - PROESTRUS FEMALES
-    estyel = fempurp(192,:); %yellow - ESTROUS FEMALES
-    metred = fempurp(128,:); %magenta? - METESTRUS FEMALES
-digreen = colorcet('L9'); %linear blue green yellow white
-    digreen = digreen(128,:); %deep green - DIESTRUS FEMALES
-clear fempurp
-maleblue = colorcet('L6'); %linear blue 192
-maleblue = maleblue(192,:); % MALES
+%fempurp = colorcet('L8'); %linear blue magenta yellow
+%    propurp = fempurp(65,:); %purple - PROESTRUS FEMALES / POOLED FEMALES
+%    estyel = fempurp(192,:); %yellow - ESTROUS FEMALES
+%    metred = fempurp(128,:); %magenta? - METESTRUS FEMALES
+%digreen = colorcet('L9'); %linear blue green yellow white
+%    digreen = digreen(128,:); %deep green - DIESTRUS FEMALES
+%clear fempurp
+%maleblue = colorcet('L6'); %linear blue 192
+%maleblue = maleblue(192,:); % MALES
+
+maleblue = [0.171931	0.744122	0.988253];
+propurp = [0.490079	0.06569	0.568432];
+digreen = [0.295855	0.606869	0.258899];
+estyellow = [0.982021	0.630867	0.240179];
+metred = [0.906173	0.1939	0.445214];
 
 % *****************************************************************************
 
