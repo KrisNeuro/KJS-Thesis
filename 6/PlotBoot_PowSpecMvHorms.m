7cd26acf79f02594e246d93bcc786ce70cebaf1b
Kris S <37545869+KrisNeuro@users.noreply.github.com>
2020-04-06 12:51:05 -0400

    Add files via upload

---
+++ 6/PlotBoot_PowSpecMvHorms.m
@@ -0,0 +1,190 @@
+%PlotBoot_PowSpecMvHorms
+% [H1,H2,H3,H4,ILstat,PLstat,DHstat,VHstat] = PlotBoot_PowSpecMvHorms(f,A,Dil,Pil,Eil,Mil,Ddh,Pdh,Edh,Mdh,Dvh,Pvh,Evh,Mvh,Dpl,Ppl,Epl,Mpl)
+% 
+% Plots Power spectra: Males vs females/hormone state, with shading 
+% representing SEM, calculated within-subject for each hormone category
+% (D,E,P). Metestrus is excluded.
+% 
+% Calls on scripts:
+%   - shadedErrorBar.m  (https://www.mathworks.com/matlabcentral/fileexchange/26311-raacampbell-shadederrorbar)
+%   - ksampL2.m 
+% 
+% Calls data produced by:
+%   - PowSpec5to15_Hormones.m: 'PxxHormones-BL-5to15.mat' (called w/in Thesis6_Format4Bootstrap_5to15.m)
+%   - Male Pxx data: Pxx-BL-5to15.mat
+% 
+% Called by:
+%   - Thesis6_Format4Bootstrap_5to15.m
+% 
+% KJS init: 2020-04-06 
+
+function [H1,H2,H3,H4,ILstat,PLstat,DHstat,VHstat] = PlotBoot_PowSpecMvHorms(f,A,Dil,Pil,Eil,Mil,Ddh,Pdh,Edh,Mdh,Dvh,Pvh,Evh,Mvh,Dpl,Ppl,Epl,Mpl)
+
+%% Setup
+
+% Params for ksampL2.m
+method = 1;
+biasflag = 1;
+
+% Color maps
+maleblue = [0 0 0]/255; %MALES  (black)
+digreen = [214 156 78]/255; %DIESTRUS FEMALES
+propurp = [4 108 154]/255; %PROESTRUS FEMALES 
+estyel = [171 221 222]/255; % ESTROUS FEMALES
+
+
+%% Plot mPFC-IL
+H1 = figure('units','normalized','outerposition',[0 0 1 1]);
+
+p3 = shadedErrorBar(f,pow2db(Eil),{@mean,@(x) std(pow2db(Eil))/sqrt(size(Eil,1))},'lineprops',{'color',estyel});
+p3.mainLine.LineWidth = 3;
+p3.mainLine.DisplayName = 'Estrus';
+hold on
+
+p2 = shadedErrorBar(f,pow2db(Pil),{@mean,@(x) std(pow2db(Pil))/sqrt(size(Pil,1))},'lineprops',{'color',propurp}); 
+p2.mainLine.LineWidth = 3;
+p2.mainLine.DisplayName = 'Proestrus';
+
+p1 = shadedErrorBar(f,pow2db(Dil),{@mean,@(x) std(pow2db(Dil))/sqrt(size(Dil,1))},'lineprops',{'color',digreen}); 
+p1.mainLine.LineWidth = 3;
+p1.mainLine.DisplayName = 'Diestrus';
+
+p4 = shadedErrorBar(f,pow2db(Mil),{@mean,@(x) std(pow2db(Mil))/sqrt(size(Mil,1))},'lineprops',{'color',maleblue}); 
+p4.mainLine.LineWidth = 3;
+p4.mainLine.DisplayName = 'Male';
+
+axis square
+xlim([0.5 100])
+xticks(10:10:100)
+xlabel('Frequency (Hz)')
+ylabel('Power (dB)')
+title('mPFC-IL')
+
+% legend([p4.mainLine, p1.mainLine, p2.mainLine, p3.mainLine],{'Male','Diestrus','Proestrus','Estrus'})
+set(gca,'fontsize',26)
+box off
+set(gca,'TitleFontSizeMultiplier',1.75)
+
+% One-way ANOVA for functional data: Main-effects test
+y1 = [Dil; Pil; Eil; Mil];
+yy = [A, y1];
+[ILstat.MvHorms]=ksampL2(yy,method,biasflag);
+clear y1 yy
+
+%% Plot mPFC-PL
+H2 = figure('units','normalized','outerposition',[0 0 1 1]);
+
+p3 = shadedErrorBar(f,pow2db(Epl),{@mean,@(x) std(pow2db(Epl))/sqrt(size(Epl,1))},'lineprops',{'color',estyel}); 
+p3.mainLine.LineWidth = 3;
+p3.mainLine.DisplayName = 'Estrus';
+hold on
+
+p2 = shadedErrorBar(f,pow2db(Ppl),{@mean,@(x) std(pow2db(Ppl))/sqrt(size(Ppl,1))},'lineprops',{'color',propurp}); 
+p2.mainLine.LineWidth = 3;
+p2.mainLine.DisplayName = 'Proestrus';
+
+p1 = shadedErrorBar(f,pow2db(Dpl),{@mean,@(x) std(pow2db(Dpl))/sqrt(size(Dpl,1))},'lineprops',{'color',digreen}); 
+p1.mainLine.LineWidth = 3;
+p1.mainLine.DisplayName = 'Diestrus';
+
+p4 = shadedErrorBar(f,pow2db(Mpl),{@mean,@(x) std(pow2db(Mpl))/sqrt(size(Mpl,1))},'lineprops',{'color',maleblue}); 
+p4.mainLine.LineWidth = 3;
+p4.mainLine.DisplayName = 'Male';
+
+axis square
+xlim([0.5 100])
+xticks(10:10:100)
+xlabel('Frequency (Hz)')
+ylabel('Power (dB)')
+title('mPFC-PL')
+
+% legend([p4.mainLine, p1.mainLine, p2.mainLine, p3.mainLine],{'Male','Diestrus','Proestrus','Estrus'})
+set(gca,'fontsize',26)
+box off
+set(gca,'TitleFontSizeMultiplier',1.75)
+
+% One-way ANOVA for functional data: Main-effects test
+y1 = [Dpl; Ppl; Epl; Mpl];
+yy = [A, y1];
+[PLstat.MvHorms]=ksampL2(yy,method,biasflag);
+clear y1 yy
+
+
+%% Plot dHPC
+H3 = figure('units','normalized','outerposition',[0 0 1 1]);
+
+p3 = shadedErrorBar(f,pow2db(Edh),{@mean,@(x) std(pow2db(Edh))/sqrt(size(Edh,1))},'lineprops',{'color',estyel}); 
+p3.mainLine.LineWidth = 3;
+p3.mainLine.DisplayName = 'Estrus';
+hold on
+
+p2 = shadedErrorBar(f,pow2db(Pdh),{@mean,@(x) std(pow2db(Pdh))/sqrt(size(Pdh,1))},'lineprops',{'color',propurp}); 
+p2.mainLine.LineWidth = 3;
+p2.mainLine.DisplayName = 'Proestrus';
+
+p1 = shadedErrorBar(f,pow2db(Ddh),{@mean,@(x) std(pow2db(Ddh))/sqrt(size(Ddh,1))},'lineprops',{'color',digreen}); 
+p1.mainLine.LineWidth = 3;
+p1.mainLine.DisplayName = 'Diestrus';
+
+p4 = shadedErrorBar(f,pow2db(Mdh),{@mean,@(x) std(pow2db(Mdh))/sqrt(size(Mdh,1))},'lineprops',{'color',maleblue}); 
+p4.mainLine.LineWidth = 3;
+p4.mainLine.DisplayName = 'Male';
+
+axis square
+xlim([0.5 100])
+xticks(10:10:100)
+xlabel('Frequency (Hz)')
+ylabel('Power (dB)')
+title('dHPC')
+
+% legend([p4.mainLine, p1.mainLine, p2.mainLine, p3.mainLine],{'Male','Diestrus','Proestrus','Estrus'})
+set(gca,'fontsize',26)
+box off
+set(gca,'TitleFontSizeMultiplier',1.75)
+
+% One-way ANOVA for functional data: Main-effects test
+y1 = [Ddh; Pdh; Edh; Mdh];
+yy = [A, y1];
+[DHstat.MvHorms]=ksampL2(yy,method,biasflag);
+clear y1 yy
+
+%% Plot vHPC
+H4 = figure('units','normalized','outerposition',[0 0 1 1]);
+
+p3 = shadedErrorBar(f,pow2db(Evh),{@mean,@(x) std(pow2db(Evh))/sqrt(size(Evh,1))},'lineprops',{'color',estyel}); 
+p3.mainLine.LineWidth = 3;
+p3.mainLine.DisplayName = 'Estrus';
+hold on
+
+p2 = shadedErrorBar(f,pow2db(Pvh),{@mean,@(x) std(pow2db(Pvh))/sqrt(size(Pvh,1))},'lineprops',{'color',propurp}); 
+p2.mainLine.LineWidth = 3;
+p2.mainLine.DisplayName = 'Proestrus';
+
+p1 = shadedErrorBar(f,pow2db(Dvh),{@mean,@(x) std(pow2db(Dvh))/sqrt(size(Dvh,1))},'lineprops',{'color',digreen}); 
+p1.mainLine.LineWidth = 3;
+p1.mainLine.DisplayName = 'Diestrus';
+
+p4 = shadedErrorBar(f,pow2db(Mvh),{@mean,@(x) std(pow2db(Mvh))/sqrt(size(Mvh,1))},'lineprops',{'color',maleblue}); 
+p4.mainLine.LineWidth = 3;
+p4.mainLine.DisplayName = 'Male';
+
+axis square
+xlim([0.5 100])
+xticks(10:10:100)
+xlabel('Frequency (Hz)')
+ylabel('Power (dB)')
+title('vHPC')
+
+% legend([p4.mainLine, p1.mainLine, p2.mainLine, p3.mainLine],{'Male','Diestrus','Proestrus','Estrus'})
+set(gca,'fontsize',26)
+box off
+set(gca,'TitleFontSizeMultiplier',1.75)
+
+% One-way ANOVA for functional data: Main-effects test
+y1 = [Dvh; Pvh; Evh; Mvh];
+yy = [A, y1];
+[VHstat.MvHorms]=ksampL2(yy,method,biasflag);
+clear y1 yy
+
+
+end
---
+++ 6/PlotBoot_PowSpecMvHormsBands.m
@@ -0,0 +1,211 @@
+%PlotBoot_PowSpecMvHormsBands
+%   [h,stat] = PlotBoot_PowSpecMvHormsBands(f,Dx,Px,Ex,Mx,stat,A)
+% 
+% Plots Power spectra: Males vs females/ hormone state, with shading 
+% representing SEM, calculated within-subject for each hormone category (D,E,P)
+% Subplots indicate frequency bands of interest: Theta, delta, gamma, beta
+% 
+% Calls on scripts:
+%   - shadedErrorBar.m  (https://www.mathworks.com/matlabcentral/fileexchange/26311-raacampbell-shadederrorbar)
+%   - ksampL2.m 
+% 
+% Calls data produced by:
+%   - PowSpec5to15_Hormones.m: 'PxxHormones-BL-5to15.mat' (called w/in Thesis6_Format4Bootstrap_5to15.m)
+%   - Male Pxx data: Pxx-BL-5to15.mat
+% 
+% Called by:
+%   - Thesis6_Format4Bootstrap_5to15.m
+% 
+% 
+% KJS init: 2020-04-06
+
+function [h,stat] = PlotBoot_PowSpecMvHormsBands(f,Dx,Px,Ex,Mx,stat,A)
+%% Setup
+
+% Params for ksampL2.m
+method = 1;
+biasflag = 1;
+
+% Color maps
+maleblue = [0 0 0]/255; %MALES  (black)
+digreen = [214 156 78]/255; %DIESTRUS FEMALES
+propurp = [4 108 154]/255; %PROESTRUS FEMALES 
+estyel = [171 221 222]/255; % ESTROUS FEMALES
+
+%% Designate frequency bands
+
+% Theta
+t1 = knnsearch(f,4);
+t2 = knnsearch(f,12);
+theta = f(t1:t2); %theta band frequencies
+
+% Gamma
+g1 = knnsearch(f,30);
+g2 = knnsearch(f,80);
+gamma = f(g1:g2); %gamma band frequencies
+
+% Delta 
+d1 = knnsearch(f,1);
+d2 = knnsearch(f,4);
+delta = f(d1:d2); %delta band frequencies
+
+% Beta
+b1 = knnsearch(f,15);
+b2 = knnsearch(f,30);
+beta = f(b1:b2);
+
+%% Plot frequency bands of interest: Delta, Theta, Gamma, Beta
+[h] = figure('units','normalized','position',[0.2,0,0.597222222222222,1]);
+
+%% Delta
+subplot(221)
+p3 = shadedErrorBar(delta,pow2db(Ex(:,d1:d2)),{@mean,@(x) std(pow2db(Ex(:,d1:d2)))/sqrt(size(Ex,1))},'lineprops',{'color',estyel});
+p3.mainLine.LineWidth = 3;
+p3.mainLine.DisplayName = 'Estrus';
+hold on
+
+p2 = shadedErrorBar(delta,pow2db(Px(:,d1:d2)),{@mean,@(x) std(pow2db(Px(:,d1:d2)))/sqrt(size(Px,1))},'lineprops',{'color',propurp}); 
+p2.mainLine.LineWidth = 3;
+p2.mainLine.DisplayName = 'Proestrus';
+
+p1 = shadedErrorBar(delta,pow2db(Dx(:,d1:d2)),{@mean,@(x) std(pow2db(Dx(:,d1:d2)))/sqrt(size(Dx,1))},'lineprops',{'color',digreen}); 
+p1.mainLine.LineWidth = 3;
+p1.mainLine.DisplayName = 'Diestrus';
+
+p4 = shadedErrorBar(delta,pow2db(Mx(:,d1:d2)),{@mean,@(x) std(pow2db(Mx(:,d1:d2)))/sqrt(size(Mx,1))},'lineprops',{'color',maleblue});
+p4.mainLine.LineWidth = 3;
+p4.mainLine.DisplayName = 'Male';
+
+axis square
+xlim([0.5 4.5])
+xticks(1:4)
+xlabel('Frequency (Hz)')
+ylabel('Power (dB)')
+title('Delta band')
+
+% legend([p4.mainLine, p1.mainLine, p2.mainLine, p3.mainLine],{'Male','Diestrus','Proestrus','Estrus'})
+set(gca,'fontsize',14)
+box off
+set(gca,'TitleFontSizeMultiplier',1.5)
+
+% One-way ANOVA for functional data: Main-effects test
+y1 = [Dx(:,d1:d2); Px(:,d1:d2); Ex(:,d1:d2); Mx(:,d1:d2)];
+yy = [A, y1];
+[stat.delta]=ksampL2(yy,method,biasflag);
+clear y1 yy
+
+
+%% Theta
+subplot(222)
+p3 = shadedErrorBar(theta,pow2db(Ex(:,t1:t2)),{@mean,@(x) std(pow2db(Ex(:,t1:t2)))/sqrt(size(Ex,1))},'lineprops',{'color',estyel});
+p3.mainLine.LineWidth = 3;
+p3.mainLine.DisplayName = 'Estrus';
+hold on
+
+p2 = shadedErrorBar(theta,pow2db(Px(:,t1:t2)),{@mean,@(x) std(pow2db(Px(:,t1:t2)))/sqrt(size(Px,1))},'lineprops',{'color',propurp}); 
+p2.mainLine.LineWidth = 3;
+p2.mainLine.DisplayName = 'Proestrus';
+
+p1 = shadedErrorBar(theta,pow2db(Dx(:,t1:t2)),{@mean,@(x) std(pow2db(Dx(:,t1:t2)))/sqrt(size(Dx,1))},'lineprops',{'color',digreen}); 
+p1.mainLine.LineWidth = 3;
+p1.mainLine.DisplayName = 'Diestrus';
+
+p4 = shadedErrorBar(theta,pow2db(Mx(:,t1:t2)),{@mean,@(x) std(pow2db(Mx(:,t1:t2)))/sqrt(size(Mx,1))},'lineprops',{'color',maleblue}); 
+p4.mainLine.LineWidth = 3;
+p4.mainLine.DisplayName = 'Metestrus';
+
+axis square
+xlim([3.5 12.5])
+xticks(4:12)
+xlabel('Frequency (Hz)')
+ylabel('Power (dB)')
+title('Theta band')
+
+% legend([p4.mainLine, p1.mainLine, p2.mainLine, p3.mainLine],{'Male','Diestrus','Proestrus','Estrus'})
+% legend('location','southwest')
+set(gca,'fontsize',14)
+box off
+set(gca,'TitleFontSizeMultiplier',1.5)
+
+% One-way ANOVA for functional data: Main-effects test
+y1 = [Dx(:,t1:t2); Px(:,t1:t2); Ex(:,t1:t2); Mx(:,t1:t2)];
+yy = [A, y1];
+[stat.theta]=ksampL2(yy,method,biasflag);
+clear y1 yy
+
+%% Beta
+subplot(223)
+p3 = shadedErrorBar(beta,pow2db(Ex(:,b1:b2)),{@mean,@(x) std(pow2db(Ex(:,b1:b2)))/sqrt(size(Ex,1))},'lineprops',{'color',estyel});
+p3.mainLine.LineWidth = 3;
+p3.mainLine.DisplayName = 'Estrus';
+hold on
+
+p2 = shadedErrorBar(beta,pow2db(Px(:,b1:b2)),{@mean,@(x) std(pow2db(Px(:,b1:b2)))/sqrt(size(Px,1))},'lineprops',{'color',propurp}); 
+p2.mainLine.LineWidth = 3;
+p2.mainLine.DisplayName = 'Proestrus';
+
+p1 = shadedErrorBar(beta,pow2db(Dx(:,b1:b2)),{@mean,@(x) std(pow2db(Dx(:,b1:b2)))/sqrt(size(Dx,1))},'lineprops',{'color',digreen}); 
+p1.mainLine.LineWidth = 3;
+p1.mainLine.DisplayName = 'Diestrus';
+
+p4 = shadedErrorBar(beta,pow2db(Mx(:,b1:b2)),{@mean,@(x) std(pow2db(Mx(:,b1:b2)))/sqrt(size(Mx,1))},'lineprops',{'color',maleblue});
+p4.mainLine.LineWidth = 3;
+p4.mainLine.DisplayName = 'Male';
+
+axis square
+xlim([14.5 30.5])
+xticks(15:5:30)
+xlabel('Frequency (Hz)')
+ylabel('Power (dB)')
+title('Beta band')
+
+% legend([p4.mainLine, p1.mainLine, p2.mainLine, p3.mainLine],{'Male','Diestrus','Proestrus','Estrus'})
+set(gca,'fontsize',14)
+box off
+set(gca,'TitleFontSizeMultiplier',1.5)
+
+% One-way ANOVA for functional data: Main-effects test
+y1 = [Dx(:,b1:b2); Px(:,b1:b2); Ex(:,b1:b2); Mx(:,b1:b2)];
+yy = [A, y1];
+[stat.beta]=ksampL2(yy,method,biasflag);
+clear y1 yy
+
+%% Gamma (broadband) 
+subplot(224)
+p3 = shadedErrorBar(gamma,pow2db(Ex(:,g1:g2)),{@mean,@(x) std(pow2db(Ex(:,g1:g2)))/sqrt(size(Ex,1))},'lineprops',{'color',estyel});
+p3.mainLine.LineWidth = 3;
+p3.mainLine.DisplayName = 'Estrus';
+hold on
+
+p2 = shadedErrorBar(gamma,pow2db(Px(:,g1:g2)),{@mean,@(x) std(pow2db(Px(:,g1:g2)))/sqrt(size(Px,1))},'lineprops',{'color',propurp}); 
+p2.mainLine.LineWidth = 3;
+p2.mainLine.DisplayName = 'Proestrus';
+
+p1 = shadedErrorBar(gamma,pow2db(Dx(:,g1:g2)),{@mean,@(x) std(pow2db(Dx(:,g1:g2)))/sqrt(size(Dx,1))},'lineprops',{'color',digreen}); 
+p1.mainLine.LineWidth = 3;
+p1.mainLine.DisplayName = 'Diestrus';
+
+p4 = shadedErrorBar(gamma,pow2db(Mx(:,g1:g2)),{@mean,@(x) std(pow2db(Mx(:,g1:g2)))/sqrt(size(Mx,1))},'lineprops',{'color',maleblue});
+p4.mainLine.LineWidth = 3;
+p4.mainLine.DisplayName = 'Male';
+
+axis square
+xlim([25 85])
+xticks(30:10:80)
+xlabel('Frequency (Hz)')
+ylabel('Power (dB)')
+title('Gamma band')
+% legend([p4.mainLine, p1.mainLine, p2.mainLine, p3.mainLine],{'Male','Diestrus','Proestrus','Estrus'})
+set(gca,'fontsize',14)
+box off
+set(gca,'TitleFontSizeMultiplier',1.5)
+
+% One-way ANOVA for functional data: Main-effects test
+y1 = [Dx(:,g1:g2); Px(:,g1:g2); Ex(:,g1:g2); Mx(:,g1:g2)];
+yy = [A, y1];
+[stat.gamma]=ksampL2(yy,method,biasflag);
+clear y1 yy
+
+
+end %function
+
