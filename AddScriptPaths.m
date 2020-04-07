8a5bb7f1211183fc5a5550db3572414f770697c0
Kris S <37545869+KrisNeuro@users.noreply.github.com>
2020-04-07 14:33:58 -0400

    Update AddScriptPaths.m

--- AddScriptPaths.m
+++ AddScriptPaths.m
@@ -3,6 +3,16 @@ function [] = AddScriptPaths()
 %
 % Adds required function scripts and dependencies for KJS/Thesis data analysis.
 %
+% 	Open-source functions/packages:		SOURCE:
+%	- chronux 				http://chronux.org/
+%	- colorcet 				https://peterkovesi.com/projects/colourmaps/
+%	- export_fig 				https://github.com/altmany/export_fig
+%	- FMAToolbox				http://fmatoolbox.sourceforge.net/
+%	- padcat				https://www.mathworks.com/matlabcentral/fileexchange/22909-padcat
+%	- removePLI				https://github.com/mrezak/removePLI
+%	- shadedErrorBar			https://www.mathworks.com/matlabcentral/fileexchange/26311-raacampbell-shadederrorbar
+%	- regoutliers 				https://www.mathworks.com/matlabcentral/fileexchange/37212-regression-outliers
+%
 %
 % KJS init: 2020-04-07
 %
@@ -20,11 +30,13 @@ sxpts = {'colorcet.m'
 'ImportVTEPM.m' 
 'LinearVelocity.m'
 'NLXNameCheck.m'
+'padcat.m'
 'PowerEnvelope.m'
 'PowSpec.m' 
 'Range.m'
 'ReadCSC_TF.m'
 'ReadCSC_TF_tsd.m'
+'regoutliers.m'
 'removePLI.m' 
 'StartTime.m'
 'TotalDistanceTraveled.m' 
