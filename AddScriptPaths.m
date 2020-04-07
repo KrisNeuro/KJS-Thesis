607640630d9696b2db342dd25ad9012523d29677
Kris S <37545869+KrisNeuro@users.noreply.github.com>
2020-04-07 14:25:19 -0400

    Update AddScriptPaths.m

--- AddScriptPaths.m
+++ AddScriptPaths.m
@@ -9,45 +9,55 @@ function [] = AddScriptPaths()
 
 
 % Full list of scripts called
-sxpts = {'export_fig.m'
+sxpts = {'colorcet.m'
+'Data.m' 
+'EndTime.m'
+'export_fig.m'
 'Diff.m'
 'HilbKJS.m' 
 'ImportCSC2.m' 
 'ImportVTBL.m' 
 'ImportVTEPM.m' 
 'LinearVelocity.m'
+'NLXNameCheck.m'
+'PowerEnvelope.m'
 'PowSpec.m' 
+'Range.m'
+'ReadCSC_TF.m'
+'ReadCSC_TF_tsd.m'
 'removePLI.m' 
+'StartTime.m'
 'TotalDistanceTraveled.m' 
 'VelDist.m' 
 'VelCumDist.m'};
 
 
-% if license == "731138" %user = KJS
-    % Function scripts
-	disp('Select gitRepo root folder for Thesis .m files')
-	gitRepo = [uigetdir(pwd,'Select gitRepo root folder for Thesis .m files') filesep]; % Thesis scripts
-		addpath(genpath(gitRepo)) 
-		
-	disp('Select root .m toolbox folder')
-	tbox = [uigetdir(gitRepo,'Select root .m toolbox folder') filesep]; % toolboxes		
-		addpath([tbox 'FMAToolbox\Analyses']) % contains LinearVelocity
-		addpath([tbox 'FMAToolbox\General']) % contains Diff, a LinearVelocity dependency
-		addpath(genpath([tbox 'MouseHPC\shared\io'])) % scripts called in VT import
-        addpath([tbox 'MouseHPC\shared\util']) % scripts called in VT import
-        addpath([tbox 'MouseHPC\shared\linearize']) % scripts called in VT import
-		
-	% Check things were added
-	for i = 1:length(sxpts)
-		fullFileName = sxpts{i};
-		if ~exist(fullFileName, 'file') % File does not exist in path
-		  warningMessage = sprintf('Warning: file does not exist in path:\n%s', fullFileName);
-		  uiwait(msgbox(warningMessage));
-		  fprintf('Please select folder containing: %s\n',fullFileName)
-		  addpath(uigetdir(gitRepo,sprintf('Select folder containing: %s',fullFileName)))
-		end
-	end
+% Function scripts
+disp('Select gitRepo root folder for Thesis .m files')
+gitRepo = [uigetdir(pwd,'Select gitRepo root folder for Thesis .m files') filesep]; % Thesis scripts
+	addpath(genpath(gitRepo)) 
 
+% Toolboxes
+disp('Select root .m toolbox folder')
+tbox = [uigetdir(gitRepo,'Select root .m toolbox folder') filesep]; % toolboxes
+addpath([tbox 'export_fig']) %contains export_fig.m
+	addpath([tbox 'FMAToolbox\Analyses']) % contains LinearVelocity.m
+	addpath([tbox 'FMAToolbox\General']) % contains Diff.m, dependency of LinearVelocity 
+	addpath(genpath([tbox 'MouseHPC\shared\io'])) % VT import dependencies
+addpath([tbox 'MouseHPC\shared\util']) % VT import dependencies
+addpath([tbox 'MouseHPC\shared\linearize']) % VT import dependencies
+
+
+% Check things were added
+for i = 1:length(sxpts)
+	fullFileName = sxpts{i};
+	if ~exist(fullFileName, 'file') % File does not exist in path
+	  warningMessage = sprintf('Warning: file does not exist in path:\n%s', fullFileName);
+	  uiwait(msgbox(warningMessage));
+	  fprintf('Please select folder containing: %s\n',fullFileName)
+	  addpath(uigetdir(gitRepo,sprintf('Select folder containing: %s',fullFileName)))
+	end
+end
 disp('Good to go!')
 
 end %function
